37e7b65bcc0c2dd6f20fc4eb0741fd2db4150452
oRecklessOne <ollie@olivers-mbp.home>
2017-02-20 18:45:20 +0000

    burger button working
    menu icons added and two are working

--- MainVC.swift
+++ MainVC.swift
@@ -11,15 +11,18 @@ import UIKit
 
 class MainVC: UIViewController {
     
-    var burgerIps = 0
+    var burgerIps = 128
+    var burgerCost = 1000
     var networth = 1000
-    var totalIps = 0
+    var totalIps = 128
     var timer = Timer()
     var burgerBought = false
     let formatter = NumberFormatter()
+    var IconMenuOpen = false
     
     
-    @IBOutlet weak var allIconStack: UIStackView!
+    
+    @IBOutlet weak var investImg: UIButton!
     @IBOutlet weak var buyImg: UIButton!
     @IBOutlet weak var managerImg: UIButton!
     @IBOutlet weak var unlocksImg: UIButton!
@@ -35,11 +38,12 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var burgerRing: KDCircularProgress!
   
-   
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         scaleAspectFit(image: burgerImg)
         scaleAspectFit(image: clothingImg)
         scaleAspectFit(image: carImg)
@@ -47,50 +51,46 @@ class MainVC: UIViewController {
         scaleAspectFit(image: shoppingImg)
         scaleAspectFit(image: rocketImg)
         
+        buyImg.alpha = 0
+        managerImg.alpha = 0
+        unlocksImg.alpha = 0
+        investImg.alpha = 0
     }
 
+    
     func scaleAspectFit (image: UIButton!) {
         image.imageView?.contentMode = UIViewContentMode.scaleAspectFit
     }
     
-        
+    
     
     @IBAction func burgerPressed(_ sender: UIButton) {
-        
-        // code for animating menu button, needs work!!!!
+      
+        if IconMenuOpen == false {
         UIView.animate(withDuration: 0.5, animations: {
-        self.managerImg.imageView?.center.x = (self.managerImg.imageView?.center.x)!+68
-            self.buyImg.imageView?.center.y = (self.buyImg.imageView?.center.y)!-68
-            self.unlocksImg.imageView?.center.x = (self.unlocksImg.imageView?.center.x)!-68
+        
+            self.buyImg.alpha = 1
+            self.managerImg.alpha = 1
+            self.unlocksImg.alpha = 1
+            self.investImg.alpha = 1
+            self.IconMenuOpen = true
         })
         
-        
-        if burgerBought == false {
-        networth = networth - 1000
-            burgerBought = true
+            }else if IconMenuOpen{
+            UIView.animate(withDuration: 0.5, animations: {
+                self.buyImg.alpha = 0
+                self.managerImg.alpha = 0
+                self.unlocksImg.alpha = 0
+                self.investImg.alpha = 0
+                self.IconMenuOpen = false
+            })
         }
-        
-        formatter.numberStyle = .currencyAccounting
-        networthLbl.text = "\(networth)"
-        burgerIps = burgerIps + 129685
-        burgerIpsLbl.text = "\(formatter.string(for: burgerIps)!)"
-        totalIps = totalIps + burgerIps
-        burgerRing.animate(fromAngle: 0, toAngle: 360, duration: 8.0) { completed in
-            if completed {
-                self.networth = self.networth + self.burgerIps
-                self.networthLbl.text = "\(self.networth)"
-            } else {
-                print("animation stopped, was interrupted")
-            }
-         }
-        
     }
-
     
     
     @IBAction func clothingPressed(_ sender: UIButton) {
-    }
 
+    }
  
     @IBAction func shoppingPressed(_ sender: UIButton) {
     }
@@ -110,9 +110,51 @@ class MainVC: UIViewController {
     }
     
     
+    // Menu Buttons
     
+    @IBAction func managerPressed(_ sender: UIButton) {
+    }
     
+    @IBAction func buyPressed(_ sender: UIButton) {
+        
+   formatter.numberStyle = .currencyAccounting
+        
+        burgerRing.animate(fromAngle: 0, toAngle: 360, duration: 8.0) { completed in
+            if completed {
+                self.networth = self.networth + self.burgerIps
+                self.networthLbl.text = "\(self.formatter.string(for: self.networth)!)"
+            } else {
+                print("animation stopped, was interrupted")
+            }
+        }
+    }
    
+    
+    
+    @IBAction func unlocksPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func investPressed(_ sender: UIButton) {
+        
+        formatter.numberStyle = .currencyAccounting
+        
+        networth = networth - burgerCost
+        burgerIps = burgerIps + 128
+        
+        totalIps = totalIps + burgerIps
+        
+        networthLbl.text = "\(formatter.string(for:networth)!)"
+        
+        burgerIpsLbl.text = "\(formatter.string(for: burgerIps)!)"
+        
+        burgerCost = burgerCost + 65
+    }
+    
+    
+    
+    
+    
 }
 
 
